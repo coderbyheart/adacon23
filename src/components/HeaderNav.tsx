e8b58284fcab12eea40089a3aa2b6766ea2daf45
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-02-15 00:23:38 +0100

    feat(menu): make work on mobile

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,3 +1,5 @@
+import { Menu, X } from 'lucide-preact'
+import { useState } from 'preact/hooks'
 import { styled } from 'styled-components'
 
 const FixedNav = styled.div`
@@ -13,61 +15,110 @@ const FixedNav = styled.div`
 	position: fixed;
 	top: 0;
 	left: 0;
-	width: 100%;
-	z-index: 1000;
+	width: 100vw;
+	z-index: 1001;
 `
 
-export const HeaderNav = () => (
-	<FixedNav>
-		<div class="container">
-			<header class="d-flex py-3 text-white">
-				<a
-					href="/"
-					class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none fw-bold"
-				>
-					<img
-						class="bi me-2"
-						width="40"
-						height="32"
-						src="./static/ada.svg"
-						alt="Ada Con Norway"
-					/>
-					<span class="fs-4">#adacon23</span>
-				</a>
+const MobileNavigation = styled.nav`
+	padding-top: 100px;
+	padding-bottom: 50px;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	z-index: 1000;
+	background-color: var(--color-dark-bg);
+	a {
+		color: white;
+	}
+`
 
-				<ul class="nav nav-pills">
-					<li class="nav-item">
-						<a href="#about" class="nav-link" aria-current="page">
-							About
+const navItems: [link: string, title: string, button?: boolean][] = [
+	['#about', 'About'],
+	['#speakers', 'Speakers'],
+	['#schedule', 'Schedule'],
+	['#venue', 'Venue'],
+	['#sponsors', 'Sponsors'],
+	['/tickets', 'Tickets', true],
+]
+
+export const HeaderNav = () => {
+	const [menuVisible, showMenu] = useState<boolean>(false)
+	return (
+		<>
+			<FixedNav>
+				<div class="container">
+					<header class="d-flex py-3 text-white justify-content-between">
+						<a
+							href="/"
+							class="d-flex align-items-center text-decoration-none fw-bold"
+						>
+							<img
+								class="bi me-2"
+								width="40"
+								height="32"
+								src="./static/ada.svg"
+								alt="Ada Con Norway"
+							/>
+							<span class="fs-4">#adacon23</span>
 						</a>
-					</li>
-					<li class="nav-item">
-						<a href="#speakers" class="nav-link">
-							Speakers
-						</a>
-					</li>
-					<li class="nav-item">
-						<a href="#schedule" class="nav-link">
-							Schedule
-						</a>
-					</li>
-					<li class="nav-item">
-						<a href="#venue" class="nav-link">
-							Venue
-						</a>
-					</li>
-					<li class="nav-item">
-						<a href="#sponsors" class="nav-link">
-							Sponsors
-						</a>
-					</li>
-					<li class="nav-item">
-						<a class="btn btn-secondary" href="/tickets">
-							Tickets
-						</a>
-					</li>
-				</ul>
-			</header>
-		</div>
-	</FixedNav>
-)
+
+						<ul class="nav nav-pills d-none d-lg-flex">
+							{navItems.map(([link, title, button]) => (
+								<li class="nav-item">
+									<a
+										class={button === true ? 'btn btn-secondary' : 'nav-link'}
+										aria-current="page"
+										href={link}
+									>
+										{title}
+									</a>
+								</li>
+							))}
+						</ul>
+
+						<button
+							type="button"
+							class="btn btn-link text-white d-lg-none mr-4"
+							onClick={() => {
+								showMenu((s) => !s)
+							}}
+							title="Toggle menu"
+						>
+							{!menuVisible && <Menu />}
+							{menuVisible && <X />}
+						</button>
+					</header>
+				</div>
+			</FixedNav>
+			{menuVisible && (
+				<div class="d-lg-none">
+					<MobileNavigation>
+						<div class="container">
+							<ul class="nav flex-column">
+								{navItems.map(([link, title, button]) => (
+									<li class="nav-item">
+										<a
+											class={
+												button === true
+													? 'btn btn-secondary ms-3 mt-2 mb-4'
+													: 'nav-link'
+											}
+											aria-current="page"
+											href={link}
+											onClick={() => {
+												showMenu(false)
+											}}
+										>
+											{title}
+										</a>
+									</li>
+								))}
+							</ul>
+						</div>
+					</MobileNavigation>
+				</div>
+			)}
+		</>
+	)
+}
