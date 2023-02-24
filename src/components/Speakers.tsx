bbb4b5833cf22be4ed473e1ccb8ed6982f3418ee
Markus Tacker <m@coderbyheart.com>
2023-02-24 14:31:03 +0100

    fix(speakers): mention workshops and topics

--- src/components/Speakers.tsx
+++ src/components/Speakers.tsx
@@ -7,8 +7,8 @@ export const Speakers = () => (
 			<div class="row py-lg-5">
 				<div class="col-md-8 offset-md-2">
 					<p>
-						Help us build an amazing line-up by tipping us great speakers using
-						our{' '}
+						Help us build an amazing line-up by tipping us great speakers and
+						workshop facilitators using our{' '}
 						<a href="https://forms.gle/53atUvDmZvn3ypTc6" target="_blank">
 							speaker submission form
 						</a>
@@ -18,10 +18,10 @@ export const Speakers = () => (
 						We plan a full day conference with talks and workshops. We are
 						looking for members of the tech scene in Norway that are hands-on
 						creators, builders, and makers of technology. We want to see those
-						who love to share about a project that matters to them and that the
-						audience will benefit from hearing about. We do not care about
-						experience as a speaker. We will pair all our speakers with a
-						personal coach to help prepare for the event.
+						who love to share about a topic or a project that matters to them
+						and that the audience will benefit from learning about. We do not
+						care about experience as a speaker. We will pair all our speakers
+						with a personal coach to help prepare for the event.
 					</p>
 				</div>
 			</div>
