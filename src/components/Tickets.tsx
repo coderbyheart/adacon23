4eaf34bcf27691344c0b6189f2699a2c989d2119
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-03-02 23:20:51 +0100

    fix(tickets): add registration start date, notification form

--- src/components/Tickets.tsx
+++ src/components/Tickets.tsx
@@ -4,9 +4,17 @@ export const Tickets = () => (
 			<div class="row text-center py-4">
 				<h2 class="py-4">Tickets</h2>
 			</div>
-			<div class="row py-lg-5">
+			<div class="row pb-lg-5">
 				<div class="col text-center">
-					<p>Tickets are not yet available, but they will be free.</p>
+					<p>Ada Con Norway 2023 attendance will be free.</p>
+					<p>The registration open on the 1st of September 2023.</p>
+					<p>
+						If you want to get notified once the ticket registration opens,{' '}
+						<a href="https://forms.gle/xZUiepTYBAbmV49u8" target="_blank">
+							please fill out this form
+						</a>
+						.
+					</p>
 				</div>
 			</div>
 		</div>
